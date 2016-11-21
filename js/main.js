1a2951a613d1711364ceec1805b837aef8d10db3
Prashanth <prashanthca93@gmail.com>
2016-11-21 22:26:25 +0530

    Image

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 		$(this).find(".portfolio-item-hover").hide();
 	});
 	$("#portfolio-items .portfolio-item").click(function(){
-		var details = portfolios[$(this).attr("id")];
-		$("#popup_container").empty().append("<span id='popup_close'></span><div class='wrapper' id='portfolio_item_details_container'><div id='portfolio_item_details'><div id='portfolio_item_details_info'><span class='image'></span><div class='image_desc'>"+details.desc+"<span class='image_desc_title'>"+details.name+"</span><span class='image_desc_link'><a href='"+details.link+"'>"+details.link+"</a></span><p class='image_desc_text'></p></div></div><div id='portfolio_item_details_ss'><div id='portfolio_ss_slideshow'><img src='/img/portfolios_ss/"+details.ss+".jpg' class='portfolio_ss_slideshow_item'/></div></div></div></div>").attr("state", "o").show();
+		var id = $(this).attr("id");
+		var details = portfolios[id];
+		$("#popup_container").empty().append("<span id='popup_close'></span><div class='wrapper' id='portfolio_item_details_container'><div id='portfolio_item_details'><div id='portfolio_item_details_info'><span class='image' style='background-image:url(/img/portfolios/"+id+"_thumb.png)'></span><div class='image_desc'>"+details.desc+"<span class='image_desc_title'>"+details.name+"</span><span class='image_desc_link'><a href='"+details.link+"'>"+details.link+"</a></span><p class='image_desc_text'></p></div></div><div id='portfolio_item_details_ss'><div id='portfolio_ss_slideshow'><img src='/img/portfolios_ss/"+details.ss+".jpg' class='portfolio_ss_slideshow_item'/></div></div></div></div>").attr("state", "o").show();
 		$("#portfolio_ss_slideshow").jScrollPane({
 			contentWidth: '800px'
 		});
