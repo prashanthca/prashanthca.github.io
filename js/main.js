1e1fc56e8a318a3c01f4b7e170f4602b9b188ef0
Prashanth <prashanthca93@gmail.com>
2016-11-24 20:43:10 +0530

    jScroll

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-var portfolios = {"eventscell":{"name":"EventsCell.com", "link":"http://eventscell.com", "desc":"", "ss": "eventscell_ss"}, "bsi": {"name":"Building Stock Inventory", "link":"", "desc":"", "ss": "bsi_ss"}, "charity": {"name":"EventsCell.com", "link":"", "desc":"", "ss": "charity_ss"}, "derextech":{"name":"DerexTech.com", "link":"http://derextech.com", "desc":"", "ss": "derextech_ss"} };
+var portfolios = {"eventscell":{"name":"EventsCell.com", "link":"http://eventscell.com", "desc":"", "ss": "eventscell_ss"}, "bsi": {"name":"Building Stock Inventory", "link":"", "desc":"", "ss": "bsi_ss"}, "charity": {"name":"Charity Web Design", "link":"", "desc":"", "ss": "charity_ss"}, "derextech":{"name":"DerexTech.com", "link":"http://derextech.com", "desc":"", "ss": "derextech_ss"} };
 $(document).ready(function(){
 	$("#portfolio-items .portfolio-item").mouseover(function(){
 		$(this).find(".portfolio-item-hover").show();
 	}).mouseout(function(){
 		$(this).find(".portfolio-item-hover").hide();
 	});
-	$("#portfolio_ss_slideshow").jScrollPane({
-		contentWidth: '800px',
-		autoReinitialise: true
-	});
+
 	$("#portfolio-items .portfolio-item").click(function(){
 		var id = $(this).attr("id");
 		var details = portfolios[id];
 		$("#popup_container").empty().append("<span id='popup_close'></span><div class='wrapper' id='portfolio_item_details_container'><div id='portfolio_item_details'><div id='portfolio_item_details_info'><span class='image' style='background-image:url(/img/portfolios/"+id+"_thumb.png)'></span><div class='image_desc'>"+details.desc+"<span class='image_desc_title'>"+details.name+"</span><span class='image_desc_link'><a href='"+details.link+"'>"+details.link+"</a></span><p class='image_desc_text'></p></div></div><div id='portfolio_item_details_ss'><div id='portfolio_ss_slideshow'><img src='/img/portfolios_ss/"+details.ss+".jpg' class='portfolio_ss_slideshow_item'/></div></div></div></div>").attr("state", "o").show();
-		
+		$("#portfolio_ss_slideshow").jScrollPane({
+			contentWidth: '800px',
+			autoReinitialise: true
+		});
 		$("#popup_close").click(function(){
 			if($("#popup_container").attr("state") == "o")
 			{
